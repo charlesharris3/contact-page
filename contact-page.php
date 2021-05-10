7edb438cc2b9605f1a8dc7e56dc56ae8e10cb19a
dainternetexplorer <c.harris3@ymail.com>
2021-05-10 13:42:25 -0700

    changed email so personal will not be used

--- contact-page.php
+++ contact-page.php
@@ -1,5 +1,5 @@
 <?php
-    $recipient = "c.harris3@ymail.com";
+    $recipient = "example@email.com";
     $name = $_POST["full-name"];
     $phone_number = $_POST["phone-number"];
     $state = $_POST["state"];
@@ -7,7 +7,7 @@
     $email = $_POST["email"];
     $selection = $_POST["contact-page-dropdown"];
     $message = $_POST["contact-page-message"];
-    $subject = $name." from ".$city."," .$state." would like to speak about ".$selection;
-    $emailMessage = "From: ".$name ."-". $email."\n Message: ".$message;    
+    $subject = $name." from ".$city.",".$state." would like to speak about ".$selection;
+    $emailMessage = "From: ".$name ."-".$email."\n Message: ".$message;    
     mail($recipient,$subject,$emailMessage) or die("There was a problem sending the email.");
 ?>
